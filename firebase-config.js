c1f73a066d2a4f87d435c12fca17410d1b0c3544
Simon Koenig <koenigg.simon@gmail.com>
2024-08-04 19:40:08 -0300

    primer commit

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAVgpHoaYV0qFTYPM3jnLtuY3XHpI7DVlk",
+  authDomain: "tdah-ef69a.firebaseapp.com",
+  projectId: "tdah-ef69a",
+  storageBucket: "tdah-ef69a.appspot.com",
+  messagingSenderId: "186599533071",
+  appId: "1:186599533071:web:7cdfe41da68034c615d17a"
+};
