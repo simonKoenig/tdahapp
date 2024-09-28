31b20645a5efcfbe3ed110c7a07163e135904c99
Santiago Di Camillo <dicamillosantiago@gmail.com>
2024-09-28 16:18:50 -0300

    Reutilizo el componente Item para las materias y recompensas

--- Components/RewardItem.js
+++ Components/RewardItem.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Item from './Item'; // Asegúrate de importar el componente
 
 const RewardItem = ({ item, onPress }) => {
+    console.log(item);
     return (
         <Item item={item} onPress={onPress} tipo="dificultad" valor={item.dificultad} />
     );
 };
 
-
 export default RewardItem;
-
--- Components/SubjectItem.js
+++ Components/SubjectItem.js
@@ -1,48 +1,11 @@
-
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Item from './Item'; // Asegúrate de importar el componente
 
-const RewardItem = ({ item, onPress }) => {
+const SubjectItem = ({ item, onPress }) => {
     return (
-        <TouchableOpacity style={styles.itemContainer} onPress={onPress}>
-            <View style={styles.circle}>
-                <Text style={styles.circleText}>{item.nombre[0]}</Text>
-            </View>
-            <Text style={styles.itemText}>{item.nombre}</Text>
-        </TouchableOpacity>
+        <Item item={item} onPress={onPress} />
     );
 };
 
-const styles = StyleSheet.create({
-    itemContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 15,
-        marginVertical: 8,
-        backgroundColor: '#f9f9f9',
-        borderRadius: 10,
-        elevation: 3,
-        height: 70,
-    },
-    circle: {
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-        backgroundColor: '#4c669f',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginRight: 15,
-    },
-    circleText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-    itemText: {
-        flex: 1,
-        fontSize: 16,
-    },
-});
-
-export default RewardItem;
+export default SubjectItem;
 
