7ad10783eaa671dba1ee9d451c68cc5cdbe41e2e
Enzo Defranco <enzo.defranco@alu.frlp.utn.edu.ar>
2024-10-23 13:43:41 -0300

    Se agregaron etiquetas de accesibilidad al componentes de la lista de usuarios vinculados

--- Components/PacienteListItem.js
+++ Components/PacienteListItem.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { DeleteIcon } from './Icons';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
-
+import { globalStyles } from '../Utils/globalStyles'; // Importamos los estilos globales
+import { COLORS, SPACING } from '../Utils/Constant'; // Importamos constantes de colores y espaciado
 
 const PacienteListItem = ({ paciente, onDelete }) => {
     const nombreArray = paciente.nombreApellido.split(' ');
 
     const iniciales = () => {
-        // Verificar que nombreArray tenga al menos un elemento y que los elementos no sean undefined o null
         if (nombreArray.length > 0 && nombreArray[0]) {
             return nombreArray.length > 1 && nombreArray[1]
                 ? `${nombreArray[0][0]}${nombreArray[1][0]}`
                 : nombreArray[0][0]; // Si solo hay una palabra, tomar solo la primera letra
         } else {
-            return (''); // Manejar el caso donde nombreArray está vacío o tiene elementos inválidos
+            return ''; // Manejar el caso donde nombreArray está vacío o tiene elementos inválidos
         }
-    }
+    };
 
     return (
-        <View style={styles.pacienteContainer}>
-            <View style={styles.circle}>
+        <View
+            style={styles.pacienteContainer}
+            accessible={true}
+            accessibilityLabel={`Usuario: ${paciente.nombreApellido}, Correo electrónico: ${paciente.email}`}
+        >
+            {/* Ícono con las iniciales del usuario */}
+            <View style={styles.circle} accessibilityLabel={`Iniciales del usuario: ${iniciales()}`}>
                 <Text style={styles.circleText}>{iniciales()}</Text>
             </View>
-            <View>
-                <Text style={styles.pacienteText}>{paciente.nombreApellido}</Text>
-                <Text style={styles.pacienteEmail}>{paciente.email}</Text>
+
+            {/* Nombre y correo del usuario */}
+            <View style={styles.infoContainer}>
+                <Text style={globalStyles.text}>{paciente.nombreApellido}</Text>
+                <Text style={globalStyles.InfoText}>{paciente.email}</Text>
             </View>
-            <TouchableOpacity style={styles.deleteButton} onPress={() => onDelete(paciente.id)}>
+
+            {/* Botón para eliminar al usuario */}
+            <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => onDelete(paciente.id)}
+                accessibilityRole="button"
+                accessibilityLabel={`Eliminar a ${paciente.nombreApellido}`}
+                accessibilityHint="Eliminará a este usuario de la lista de usuarios vinculados"
+            >
                 <DeleteIcon size={42} color="red" />
             </TouchableOpacity>
         </View>
@@ -37,40 +52,30 @@ const styles = StyleSheet.create({
     pacienteContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        padding: 15,
-        marginVertical: 8,
-        backgroundColor: '#f9f9f9',
+        padding: SPACING.small,  // Usamos el espaciado definido en constantes
         borderRadius: 10,
-        elevation: 3,
-        height: 70,
-        position: 'relative', // Necesario para posicionar el ícono de eliminación
+        height: 60,
+        position: 'relative',  // Necesario para posicionar el ícono de eliminación
     },
     circle: {
         width: 40,
         height: 40,
         borderRadius: 20,
-        backgroundColor: '#4c669f',
+        backgroundColor: COLORS.primary,  // Usamos el color primario definido
         alignItems: 'center',
         justifyContent: 'center',
-        marginRight: 15,
+        marginRight: SPACING.small,
     },
     circleText: {
-        color: '#fff',
+        color: COLORS.secondary,  // Usamos el color de texto secundario
         fontWeight: 'bold',
         fontSize: 18,
     },
-    pacienteText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    pacienteEmail: {
-        fontSize: 14,
-        color: '#777',
-    },
     deleteButton: {
         position: 'absolute',
-        right: 10,
-        top: 'auto',
+        right: 0,
+        top: '50%',
+        transform: [{ translateY: -12 }],
     },
     infoContainer: {
         flex: 1,
