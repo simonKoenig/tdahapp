fedb8cf805ffb21b9e265213db8df4284f1e25e4
Enzo Defranco <enzo.defranco@alu.frlp.utn.edu.ar>
2024-10-26 18:48:30 -0300

    Reordenación del codigo

--- Context/TaskProvider.js
+++ Context/TaskProvider.js
@@ -16,96 +16,53 @@ export const TasksProvider = ({ children }) => {
 
     useEffect(() => {
         const loadTasks = async () => {
-            // Si el usuario no está autenticado, no hace nada
-            if (!user) {
-                return;
-            }
+            if (!user) return;
 
-            // Si el usuario es un paciente, se suscribe a sus propias recompensas en tiempo real
             if (isPaciente()) {
+                if (unsubscribe) unsubscribe();
 
-                // Cancela la suscripción anterior si existe
-                if (unsubscribe) {
-                    unsubscribe();
-                }
-
-                // Usa las recompensas en caché si existen
                 const cachedTasks = await getAsyncStorage(`tasks_${user.uid}`);
                 if (cachedTasks) {
                     console.log('Setting Tasks from cache');
-                    setTasks(sortTasks(cachedTasks)); // Ordenar las tareas antes de establecerlas en el estado
+                    setTasks(sortTasks(cachedTasks));
                 } else {
                     console.log('Fetching tasks');
                     fetchTasks(user.uid);
                 }
 
-                // Crea una referencia a la colección de recompensas del usuario
                 const tasksRef = collection(db, 'usuarios', user.uid, 'tareas');
-
-                // Se suscribe a los cambios en la colección de recompensas
                 const newUnsubscribe = onSnapshot(tasksRef, async (snapshot) => {
-                    // Convertir los documentos de Firestore en objetos
                     let tasksList = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-                    // Log antes de ordenar
-                    console.log("Tareas desde snapshot antes de ordenar:");
-                    tasksList.forEach(task => {
-                        console.log(`Tarea: ${task.nombre}, Estado: ${task.estado}, correctionDate: ${task.correccion?.correctionDate?.seconds}`);
-                    });
-
-                    // Ordenar las tareas usando la función `sortTasks`
                     const sortedTasks = sortTasks(tasksList);
-
-                    // Log después de ordenar
-                    console.log("Tareas después de ordenar desde snapshot:");
-                    sortedTasks.forEach(task => {
-                        console.log(`Tarea: ${task.nombre}, Estado: ${task.estado}, correctionDate: ${task.correccion?.correctionDate?.seconds}`);
-                    });
-
-                    // Actualizar el estado con la lista ordenada
                     setTasks(sortedTasks);
-                    console.log('Setting tasks from snapshot');
-
-                    // Guardar las tareas ordenadas en AsyncStorage
                     await setAsyncStorage(`tasks_${user.uid}`, sortedTasks);
                 });
-                // Guarda la nueva función de desuscripción
                 setUnsubscribe(() => newUnsubscribe);
-            }
-            // Si el usuario tiene rol de administrador, utiliza el caché y el fetch
-            else {
-                // Si no hay un paciente seleccionado, sale de la función
+            } else {
                 if (!selectedPatientId) {
                     console.error('No selected patient ID');
                     return;
                 }
 
-                // Cancela la suscripción anterior si existe
-                if (unsubscribe) {
-                    unsubscribe();
-                }
+                if (unsubscribe) unsubscribe();
 
-                // Usa las recompensas en caché si existen, sino las obtiene con el fetch
                 const cachedTasks = await getAsyncStorage(`tasks_${selectedPatientId}`);
                 if (cachedTasks) {
                     console.log('Setting Tasks from cache');
-                    setTasks(sortTasks(cachedTasks)); // Ordenar las tareas antes de establecerlas en el estado
+                    setTasks(sortTasks(cachedTasks));
                 } else {
                     console.log('Fetching tasks');
                     fetchTasks(selectedPatientId);
                 }
             }
 
-            // Cancela la suscripción al desmontar el componente
             return () => {
-                if (unsubscribe) {
-                    unsubscribe();
-                }
+                if (unsubscribe) unsubscribe();
             };
         };
         loadTasks();
-
     }, [selectedPatientId, user]);
+
 
     //Se usa la misma función para el paciente, donde si no se le pasa el uid, se toma el uid del paciente logueado
     const fetchTasks = async (uid = null) => {
@@ -114,30 +71,13 @@ export const TasksProvider = ({ children }) => {
             try {
                 console.log('Fetching tasks for UID:', userId);
 
-                // Crear la referencia y la consulta con la ordenación deseada en Firestore
+                // Obtener la lista de tareas desde Firestore
                 const tasksRef = collection(db, 'usuarios', userId, 'tareas');
                 const tasksSnapshot = await getDocs(tasksRef);
-                let tasksList = tasksSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                const tasksList = tasksSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
-                // Ordenar las tareas después de obtenerlas de Firestore
-                const completedTasks = tasksList
-                    .filter(task => task.estado.toLowerCase() === 'finalizada')
-                    .sort((a, b) => {
-                        // Ordenar por correctionDate de mayor a menor
-                        return (b.correccion?.correctionDate?.seconds || 0) - (a.correccion?.correctionDate?.seconds || 0);
-                    });
-
-                const uncompletedTasks = tasksList
-                    .filter(task => task.estado.toLowerCase() !== 'finalizada')
-                    .sort((a, b) => {
-                        // Ordenar por fecha para que las tareas que vencen primero estén arriba
-                        return (a.date?.seconds || 0) - (b.date?.seconds || 0);
-                    });
-
-                // Combinar las listas ordenadas
-                const finalTasks = [...uncompletedTasks, ...completedTasks];
-
-                // Actualizar el estado con las tareas ordenadas
+                // Ordenar usando `sortTasks` y actualizar el estado
+                const finalTasks = sortTasks(tasksList);
                 setTasks(finalTasks);
 
                 // Verificación de las tareas ordenadas
@@ -154,6 +94,7 @@ export const TasksProvider = ({ children }) => {
             console.error('UID or user is not defined');
         }
     };
+
     const addTask = async (task, uid) => {
         if (uid) {
             try {
@@ -162,25 +103,8 @@ export const TasksProvider = ({ children }) => {
                 const docRef = await addDoc(tasksRef, task);
                 const newTask = { id: docRef.id, ...task };
 
-                // Añadir la nueva tarea y luego ordenar todas las tareas
-                setTasks(prevTasks => {
-                    const updatedTasks = [...prevTasks, newTask];
-
-                    // Ordenar las tareas después de añadir la nueva tarea
-                    const completedTasks = updatedTasks
-                        .filter(task => task.estado.toLowerCase() === 'finalizada')
-                        .sort((a, b) => {
-                            return (b.correccion?.correctionDate?.seconds || 0) - (a.correccion?.correctionDate?.seconds || 0);
-                        });
-
-                    const uncompletedTasks = updatedTasks
-                        .filter(task => task.estado.toLowerCase() !== 'finalizada')
-                        .sort((a, b) => {
-                            return (a.date?.seconds || 0) - (b.date?.seconds || 0);
-                        });
-
-                    return [...uncompletedTasks, ...completedTasks];
-                });
+                // Añadir la nueva tarea y luego ordenar todas las tareas con `sortTasks`
+                setTasks(prevTasks => sortTasks([...prevTasks, newTask]));
 
                 // También actualizar AsyncStorage para mantener el cache actualizado
                 await addAsyncStorage(`tasks_${uid}`, newTask);
@@ -191,6 +115,7 @@ export const TasksProvider = ({ children }) => {
             console.error('No UID provided');
         }
     };
+
 
     const updateTask = async (id, updatedTask, uid = null) => {
         const userId = uid || user?.uid;
