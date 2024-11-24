75f131c98e4bb3dfcc44dc2e4f82d9213263c0e7
Santiago Di Camillo <dicamillosantiago@gmail.com>
2024-11-24 15:27:23 -0300

    Manejo de errores en el login

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
-import * as React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, AccessibilityInfo } from 'react-native';
+import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../firebase-config';
 import { useNavigation } from '@react-navigation/native';
 import { EyeIcon, EyeOffIcon } from '../Components/Icons';
+import Toast from 'react-native-toast-message';
 import { globalStyles } from '../Utils/globalStyles';
 import { PLACEHOLDER_TEXT_COLOR, SPACING } from '../Utils/Constant';
 
 
 function LoginScreen() {
     const navigation = useNavigation();
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [isPasswordVisible, setIsPasswordVisible] = React.useState(false); // Estado para la visibilidad de la contraseña
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // Estado para la visibilidad de la contraseña
+    const [errors, setErrors] = useState({});
 
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
@@ -27,6 +29,35 @@ function LoginScreen() {
     }
 
     const iniciarSesion = () => {
+        const newErrors = {};
+
+        if (email.length === 0) {
+            newErrors.email = 'El correo electrónico es obligatorio';
+        } else if (!email.includes('@') && !email.includes('.')) {
+            newErrors.email = 'El correo electrónico no es válido';
+        }
+
+        if (password.length === 0) {
+            newErrors.password = 'La contraseña es obligatoria';
+        } else if (password.length < 6) {
+            newErrors.password = 'La contraseña debe tener al menos 6 caracteres';
+        }
+
+        // Verifica si hay errores
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            
+            // Construye el mensaje para el lector de pantalla
+            const accessibilityMessage = `Error al iniciar sesión. ${Object.values(newErrors).join('. ')}`;
+
+            // Anuncia el mensaje para TalkBack o VoiceOver
+            AccessibilityInfo.announceForAccessibility(accessibilityMessage);
+
+            return;
+        } else {
+            setErrors({});
+        }
+
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log('Sesión iniciada');
@@ -34,7 +65,11 @@ function LoginScreen() {
             })
             .catch((error) => {
                 console.log(error);
-                Alert.alert(error.message);
+                Toast.show({
+                    type: 'error',
+                    text1: 'Error',
+                    text2: 'Correo electrónico o contraseña incorrectos',
+                });
             })
     }
 
@@ -48,21 +83,32 @@ function LoginScreen() {
                 <Text style={globalStyles.label} accessibilityLabel="Campo del correo electrónico">Correo electrónico</Text>
                 <TextInput 
                     accessible={true}
-                    onChangeText={(text) => setEmail(text)} 
-                    style={globalStyles.input} 
+                    onChangeText={(text) => {
+                        setEmail(text);
+                        setErrors((prevErrors) => ({ ...prevErrors, email: '' }));
+                    }} 
+                    style={[globalStyles.input, errors.email && globalStyles.errorInput]}
                     placeholderTextColor={PLACEHOLDER_TEXT_COLOR}
                     placeholder='ejemplo@mail.com'
                     value={email}
                     autoCapitalize="none"
                     keyboardType="email-address"
                 />
+                {errors.email ? (
+                    <Text style={globalStyles.errorText}>
+                        {errors.email}
+                    </Text>
+                ) : null}
                 
                 <Text style={globalStyles.label} accessibilityLabel='Campo de ingreso de contraseña'>Contraseña</Text>
                 <View style={styles.passwordContainer}>
                     <TextInput
                         accessible={true}
-                        onChangeText={(text) => setPassword(text)}
-                        style={[globalStyles.input, {width: '100%'}]}
+                        onChangeText={(text) => {
+                            setPassword(text)
+                            setErrors((prevErrors) => ({ ...prevErrors, password: '' }));
+                        }}
+                        style={[globalStyles.input, {width: '100%'} , errors.password && globalStyles.errorInput]}
                         placeholder='Ingrese su contraseña'
                         placeholderTextColor={PLACEHOLDER_TEXT_COLOR}
                         secureTextEntry={!isPasswordVisible} 
@@ -78,6 +124,11 @@ function LoginScreen() {
                         )}
                     </TouchableOpacity>
                 </View>
+                {errors.email ? (
+                    <Text style={globalStyles.errorText}>
+                        {errors.password}
+                    </Text>
+                ) : null}
                 
                 <TouchableOpacity onPress={olvideContraseña}>
                     <Text style={styles.olvidePassword}>¿Olvidó su contraseña?</Text>
